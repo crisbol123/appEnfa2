704a96241fdf1fe15b310b401167083737f8f07d
yhoys <167016213+yhoys@users.noreply.github.com>
2024-11-26 22:57:29 -0500

    Se completaron las vistas y se hicieron pequeños cambios de diseño

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from "./login/login.component";
-import { MainMenuComponent } from "./main-menu/main-menu.component";
-import { AgregarMedicoComponent } from "./agregar-medico/agregar-medico.component";
-import { PerfilComponent } from './perfil/perfil.component';
-import { AdministrarMedicoComponent } from './administrar-medico/administrar-medico.component';
+import { Router, RouterModule, RouterOutlet, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, /*LoginComponent, MainMenuComponent, AgregarMedicoComponent, PerfilComponent*/],
+  imports: [RouterModule,RouterOutlet,CommonModule,MatIconModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'appEnfa2';
+  isLoginPage: boolean = false;
+  isMainMenuPage: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isLoginPage = event.url === '/login';
+        this.isMainMenuPage = event.url === '/main-menu';
+      }
+    });
+  }
+
+  goToMainMenu(): void {
+    this.router.navigate(['/main-menu']);
+  }
 }

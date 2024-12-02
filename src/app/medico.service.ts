960198e6288b43b1109bf807c5e80f5cb96ebbd1
cristianfbolanos@unicauca.edu.co <cristianfbolanosfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git initReinitialized existing Git repository in C:/Users/felip/aplicacion/.git/felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add.git: 'add.' is not a git command. See 'git --help'.The most similar command is        addfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add .felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer commiterror: pathspec 'commit' did not match any file(s) known to gitfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer>
2024-12-02 00:33:06 -0500

    se añadio servicio de medicos

---
+++ src/app/medico.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MedicoService {
+  private apiUrl = 'http://localhost:8085/medico'; 
+
+  constructor(private http: HttpClient) {}
+
+
+  getMedicos(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/listar`);
+  }
+
+  createMedico(medico: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/guardar`, medico);
+  }
+
+
+
+  deleteMedico(id: number): Observable<any> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<any>(url);
+  }
+}
